071b910286c0d0d93f112a92522bb228ee957190
RYAN\ryan3_000 <virtualhauntedhousedallas@gmail.com>
2017-01-24 17:06:01 -0600

    BT04 Deleting Landscape

---
+++ TankUdemy/Source/TankUdemy/TankUdemyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TankUdemyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKUDEMY_API ATankUdemyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
